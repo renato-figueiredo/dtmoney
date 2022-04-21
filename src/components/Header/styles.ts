88c2869186ed3812be396e3e072891bc35899475
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2022-04-21 13:41:10 -0300

    add Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background: var(--blue);
+`;
+
+export const Content = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
+
+  padding: 2rem 1rem 12rem; //rem equivale ao tamanho do font size do root ([Desktop] 1 rem = 16px | 10rem = 160px)
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    font-size: 1rem;
+    color: #fff;
+    background: var(--blue-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    transition: filter 0.2s; //Tempo de transição apenas para o filter
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
