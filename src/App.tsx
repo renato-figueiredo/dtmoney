27b53ea39f38d9e67e6335851d2df62fc47fe24b
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2022-04-22 14:10:33 -0300

    add modal and set props in Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root'); //Sempre usar para o modal ser executado no root e não no body (visando acessibilidade)
+
 export function App() {
+  const [isNewTransactionModalOpen, setNewTransactionModalOpen] = useState(false); //Verifica se o modal está aberto e inicia seu valor como false, pois o modal inicia fechado
+
+  function handleOpenNewTransactionModal() { //handle, pois depende de uma ação do usuario (como um clique)
+    setNewTransactionModalOpen(true); //abre o modal
+  }
+
+  function handleCloseNewTransactionModal() {
+    setNewTransactionModalOpen(false); //fecha o modal
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTrasactionModal={handleOpenNewTransactionModal} /> {/* //Passar propriedade/função para enviar ao Header, no onClick */}
       <Dashboard />
+
+      <Modal 
+        isOpen={isNewTransactionModalOpen} //abre o modal
+        onRequestClose={handleCloseNewTransactionModal} // fecha o modal
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg'
+
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps { //Com uso do typescript é "bom" definir quais são as propriedade
+  onOpenNewTrasactionModal: () => void; // essa propriedade é uma função que não devolve parametro por isso void
+}
+
+export function Header({ onOpenNewTrasactionModal }: HeaderProps) { //add propriedades para receber a função do onClick do botão
   return (
     <Container>
       <Content>
       <img src={logoImg} alt="dt money" />
-      <button type="button">
+      <button type="button" onClick={onOpenNewTrasactionModal}>
         Nova Transação
       </button>
       </Content>
