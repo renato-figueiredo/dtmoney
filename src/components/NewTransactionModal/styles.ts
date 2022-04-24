c8fe1b7a335db170dcca0c648f007a7faef0de70
Renato Figueiredo <renatofigueiredoalves@gmail.com>
2022-04-24 13:04:59 -0300

    update button(deposit and withdraw) functionality

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -48,38 +48,42 @@ export const Container = styled.form`
 `;
 
 export const TransactionTypeContainer = styled.div`
-  margin: 1rem 0.5rem;
+  margin: 1rem 0;
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button { 
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
+interface RadioBoxProps {
+  isActive: boolean; //nova propriedade criada
+}
 
-    background: transparent;
+export const RadioBox = styled.button<RadioBoxProps>` //recebe a propriedade
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  background: ${(props) => props.isActive ? '#ccc' : 'transparent'}; // arrow function sem chaves para retornar automaticamente
 
-    transition: border-color 0.2s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    &:hover {
-      border-color: ${darken(0.1, '#d7d7d7')}; //biblioteca polished para utilizar o javascript para influenciar nas cores do css
-    }
+  transition: border-color 0.2s;
 
-    img { 
-      width: 20px;
-      height: 20px;
-    }
-
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')}; //biblioteca polished para utilizar o javascript para influenciar nas cores do css
   }
-`
+
+  img { 
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+`;
